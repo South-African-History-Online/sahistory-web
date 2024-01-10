1a32d405254c1c472c41aa11337429f616310ad3
Michael Karbowiak <michael@karbowiak.dk>
2024-01-10 15:15:35 +0100

    Update deploy and comment out backup for now

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 cd /web
 
 # Start backup
-echo "Starting backup"
-docker run -it -v /web:/app --network internet --rm sahistory/jobs:latest /usr/local/bin/drush status
-docker run -it -v /web:/app --network internet --rm sahistory/jobs:latest /usr/local/bin/drush sql-dump --gzip --extra-dump="--column-statistics=0" --result-file=/app/.novi/backups/saho-$(date +%Y-%m-%d-%H-%M-%S).sql.gz
-echo "Backup done"
+#echo "Starting backup"
+#docker run -it -v /web:/app --network internet --env-file /web/.env --rm sahistory/jobs:latest /usr/local/bin/drush status
+#docker run -it -v /web:/app --network internet --env-file /web/.env --rm sahistory/jobs:latest /usr/local/bin/drush sql-dump --gzip --result-file=/app/.novi/backups/saho-$(date +%Y-%m-%d-%H-%M-%S).sql
+#echo "Backup done"
 
 # Start deploy
 git pull
@@ -18,7 +18,7 @@ docker-compose start
 until docker exec -it redis redis-cli ping; do echo "Waiting for Redis to be available..."; sleep 5; done
 
 # Wait till MySQL is healthy
-until docker run -it -v /web:/app --network internet --rm sahistory/jobs:latest /usr/local/bin/drush sql:query 'SHOW TABLES;'; do echo "Waiting for DB to be available..."; sleep 5; done
+until docker run -it -v /web:/app --network internet --env-file /web/.env --rm sahistory/jobs:latest /usr/local/bin/drush sql:query 'SHOW TABLES;'; do echo "Waiting for DB to be available..."; sleep 5; done
 echo "DB is available"
 
 # Setup Solr
@@ -27,12 +27,12 @@ docker exec -it solr /opt/solr/bin/solr create_core -c saho -d /opt/solr/server/
 
 # Setup Drupal
 echo "Run database and configuration updates"
-docker run -it -v /web:/app --network internet --rm sahistory/jobs:latest /usr/local/bin/drush status
-docker run -it -v /web:/app --network internet --rm sahistory/jobs:latest /usr/local/bin/drush updb -y
-docker run -it -v /web:/app --network internet --rm sahistory/jobs:latest /usr/local/bin/drush cim -y
-docker run -it -v /web:/app --network internet --rm sahistory/jobs:latest /usr/local/bin/drush locale-check
-docker run -it -v /web:/app --network internet --rm sahistory/jobs:latest /usr/local/bin/drush locale-update
-docker run -it -v /web:/app --network internet --rm sahistory/jobs:latest /usr/local/bin/drush cr
+docker run -it -v /web:/app --network internet --env-file /web/.env --rm sahistory/jobs:latest /usr/local/bin/drush status
+docker run -it -v /web:/app --network internet --env-file /web/.env --rm sahistory/jobs:latest /usr/local/bin/drush updb -y
+docker run -it -v /web:/app --network internet --env-file /web/.env --rm sahistory/jobs:latest /usr/local/bin/drush cim -y
+docker run -it -v /web:/app --network internet --env-file /web/.env --rm sahistory/jobs:latest /usr/local/bin/drush locale-check
+docker run -it -v /web:/app --network internet --env-file /web/.env --rm sahistory/jobs:latest /usr/local/bin/drush locale-update
+docker run -it -v /web:/app --network internet --env-file /web/.env --rm sahistory/jobs:latest /usr/local/bin/drush cr
 
 # Cleanup old backups
 echo "Cleaning up old database backups older than a month"
