6254f3895763265fdcc94a38a91d8018787be0c1
Michael Karbowiak <michael@karbowiak.dk>
2024-01-10 11:16:18 +0100

    Fix the deploy job to use the jobs container and also set paths correctly

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cd /docker/beierholm-intra/
+cd /web
 
 # Start backup
 echo "Starting backup"
-docker exec -it web /usr/local/bin/drush status
-docker exec -it web /usr/local/bin/drush sql-dump --gzip --extra-dump="--column-statistics=0" --result-file=/var/www/.novi/backups/beierholm-$(date +%Y-%m-%d-%H-%M-%S).sql
+docker run -it -v /web:/app --rm sahistory/jobs:latest /usr/local/bin/drush status
+docker run -it -v /web:/app --rm sahistory/jobs:latest /usr/local/bin/drush sql-dump --gzip --extra-dump="--column-statistics=0" --result-file=/app/.novi/backups/saho-$(date +%Y-%m-%d-%H-%M-%S).sql.gz
 echo "Backup done"
 
 # Start deploy
@@ -18,25 +18,25 @@ docker-compose start
 until docker exec -it redis redis-cli ping; do echo "Waiting for Redis to be available..."; sleep 5; done
 
 # Wait till MySQL is healthy
-until docker exec -it web /usr/local/bin/drush sql:query 'SHOW TABLES;'; do echo "Waiting for DB to be available..."; sleep 5; done
+until docker run -it -v /web:/app --rm sahistory/jobs:latest /usr/local/bin/drush sql:query 'SHOW TABLES;'; do echo "Waiting for DB to be available..."; sleep 5; done
 echo "DB is available"
 
 # Setup Solr
-echo "Creating Beierholm Solr core"
-docker exec -it solr /opt/solr/bin/solr create_core -c beierholm -d /opt/solr/server/solr/beierholm -n beierholm
+echo "Creating SAHO Solr core"
+docker exec -it solr /opt/solr/bin/solr create_core -c saho -d /opt/solr/server/solr/saho -n saho
 
 # Setup Drupal
 echo "Run database and configuration updates"
-docker exec -it web /usr/local/bin/drush status
-docker exec -it web /usr/local/bin/drush updb -y
-docker exec -it web /usr/local/bin/drush cim -y
-docker exec -it web /usr/local/bin/drush locale-check
-docker exec -it web /usr/local/bin/drush locale-update
-docker exec -it web /usr/local/bin/drush cr
+docker run -it -v /web:/app --rm sahistory/jobs:latest /usr/local/bin/drush status
+docker run -it -v /web:/app --rm sahistory/jobs:latest /usr/local/bin/drush updb -y
+docker run -it -v /web:/app --rm sahistory/jobs:latest /usr/local/bin/drush cim -y
+docker run -it -v /web:/app --rm sahistory/jobs:latest /usr/local/bin/drush locale-check
+docker run -it -v /web:/app --rm sahistory/jobs:latest /usr/local/bin/drush locale-update
+docker run -it -v /web:/app --rm sahistory/jobs:latest /usr/local/bin/drush cr
 
 # Cleanup old backups
 echo "Cleaning up old database backups older than a month"
-find /docker/beierholm-intra/.novi/backups -name "beierholm-*.sql.gz" -type f -mtime +31 -delete
+find /web/.novi/backups -name "saho-*.sql.gz" -type f -mtime +31 -delete
 
 # Cleanup old images
 echo "Cleaning up dangling images - keeping image from the last 31 days"
