59cea0ec7f454516842667e376e41006e4e12ee7
Mads Nørgaard <madsnorgaard@live.dk>
2024-10-17 14:24:45 +0200

    Migrate project to Drupal 9 and apply necessary fixes

--- webroot/modules/custom/glazed_builder/README.md
+++
@@ -1,1 +0,0 @@
-For documentation and support please check out our online help center: https://www.sooperthemes.com/hc
--- webroot/sites/sites.php
+++ webroot/sites/sites.php
@@ -1,7 +1,5 @@
 <?php
 
-$sites[getenv('DOMAIN')] = getenv('WEBROOT_SITE');
-
 if (file_exists(__DIR__ . '/sites.local.php')) {
     include __DIR__ . '/sites.local.php';
 };
