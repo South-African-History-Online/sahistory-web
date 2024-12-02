2cb0cfd9fdc25dbce4dd63103645d78e406bb0ed
Mads Nørgaard <madsnorgaard@live.dk>
2024-12-02 10:03:50 +0100

    d8 make worky

--- webroot/sites/sites.php
+++ webroot/sites/sites.php
@@ -1,6 +1,7 @@
 <?php
 
 $sites[getenv('DOMAIN')] = getenv('WEBROOT_SITE');
+$sites['saho.novicell.dev'] = 'default';
 
 if (file_exists(__DIR__ . '/sites.local.php')) {
     include __DIR__ . '/sites.local.php';
