446cee0b3552db915c12218405c1aec4d74dabb9
Mads Nørgaard <madsnorgaard@live.dk>
2025-02-08 21:20:48 +0100

    Add layout builder layout module and work on TDIH block

---
+++ webroot/modules/custom/saho_utils/tdih/templates/components/tdih/tdih.js
@@ -0,0 +1,8 @@
+(function ($, Drupal) {
+    Drupal.behaviors.tdihBlock = {
+      attach: function (context, settings) {
+        console.log('This Day in History block attached.');
+      },
+    };
+  })(jQuery, Drupal);
+  
