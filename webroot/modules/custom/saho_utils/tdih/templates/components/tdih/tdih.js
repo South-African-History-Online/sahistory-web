bfd6f945b19eebe013673d73bbf63cd890358ec1
Mads Nørgaard <madsnorgaard@live.dk>
2025-02-20 20:52:09 +0100

    Add saho style guide colours and light backend theme to layout builder

--- webroot/modules/custom/saho_utils/tdih/templates/components/tdih/tdih.js
+++
@@ -1,8 +0,0 @@
-(function ($, Drupal) {
-    Drupal.behaviors.tdihBlock = {
-      attach: function (context, settings) {
-        console.log('This Day in History block attached.');
-      },
-    };
-  })(jQuery, Drupal);
-  
